b1d4df3eb138229136bb101ce6a41016adbdfcb7
Alex <alex@pop-os.localdomain>
2019-05-21 22:21:03 +0100

    Fixes and tweaks for folio

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import classnames from "classnames";
 
-import { aboutPayload } from "../../data";
+import { aboutPayload } from "../../common/data";
 
 import * as commonStyles from "../../style/common.css";
 
