8b123623bc03ea121885fa54218cfb7ba5b7b9f2
alexbrown201 <alex.brown@photobox.com>
2021-01-17 21:53:57 +0000

    Fixed webpack build

--- webpack/client/index.js
+++ webpack/client/index.js
@@ -1,8 +1,3 @@
-console.log('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
-console.log('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
-console.log('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
-console.log(process.env.NODE_ENV)
-
 const enviroment = process.env.NODE_ENV || 'development';
 
 const { config } = require(`./client.${enviroment}`);
--- webpack/serverless/index.js
+++ webpack/serverless/index.js
@@ -1,10 +1,5 @@
 const enviroment = process.env.NODE_ENV || 'development';
 
-console.log('***************************************************')
-console.log('***************************************************')
-console.log('***************************************************')
-console.log(enviroment)
-
 const { config } = require(`./serverless.${enviroment}`);
 
 export { config }
