b61d3a499fdfffbbfcf7414e1bb714345eda5b7f
Alex Brown <alex@devnotnull.com>
2021-11-12 20:06:45 +0000

    Updated build process

--- webpack/serverless/index.js
+++ webpack/serverless/index.js
@@ -2,8 +2,6 @@ const merge = require('webpack-merge');
 
 const enviroment = process.env.NODE_ENV || 'development';
 
-const clientConfig = require(`../client/client.${enviroment}`);
 const serverConfig = require(`./serverless.${enviroment}`);
 
 module.exports = serverConfig
-// module.exports = [serverConfig, clientConfig]
