27d8b1fed26be69e18c1dce55bbc69239fb4e784
Alex Brown <alexbrown201@googlemail.com>
2024-01-22 21:39:00 +0000

    feat Add back in meta viewport tag

--- src/web/components/seo/seo.tsx
+++ src/web/components/seo/seo.tsx
@@ -10,11 +10,15 @@ export const SEO = ({ title, description, name, type }) => {
       <meta property="og:type" content={type} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
+      <meta property="description" content={description} />
 
       <meta name="twitter:creator" content={name} />
       <meta name="twitter:card" content={type} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
+
+      <meta name="robots" content="index, follow" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </Helmet>
   );
 };
